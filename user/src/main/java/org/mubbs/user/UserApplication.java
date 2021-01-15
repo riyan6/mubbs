5cc0b7f3d03fcc6cfbaa07f87f5eb9b9df20e480
riyan6 <751696921@qq.com>
2021-01-15 15:08:32 +0800

    优化

--- user/src/main/java/org/mubbs/user/UserApplication.java
+++ user/src/main/java/org/mubbs/user/UserApplication.java
@@ -2,6 +2,7 @@ package org.mubbs.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import tk.mybatis.spring.annotation.MapperScan;
 
 /**
@@ -11,6 +12,7 @@ import tk.mybatis.spring.annotation.MapperScan;
  */
 @MapperScan("org.mubbs.user.dao")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserApplication {
 
     public static void main(String[] args) {
